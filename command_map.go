5b0df02e2af8ae582545784f5d3549916a8dcb18
Thomas Daniel <daniel_thomas1@outlook.fr>
2026-03-15 14:26:10 +0100

    Add working commandmap, missing feature incremental by 20 each call of map

---
+++ command_map.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const locationsAreas = "https://pokeapi.co/api/v2/location-area"
+
+type locationAreaAPIResponse struct {
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	GameIndex int `json:"game_index"`
+	ID        int `json:"id"`
+	Location  struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Name  string `json:"name"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			EncounterDetails []struct {
+				Chance          int   `json:"chance"`
+				ConditionValues []any `json:"condition_values"`
+				MaxLevel        int   `json:"max_level"`
+				Method          struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+				MinLevel int `json:"min_level"`
+			} `json:"encounter_details"`
+			MaxChance int `json:"max_chance"`
+			Version   struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"pokemon_encounters"`
+}
+
+func commandMap() error {
+
+	limit := 20
+	client := &http.Client{Timeout: 3 * time.Second}
+
+	for i := 0; i < limit; i++ {
+		fullURL := locationsAreas + "/" + strconv.Itoa(i+1)
+
+		req, err := http.NewRequest(http.MethodGet, fullURL, nil)
+		if err != nil {
+			return fmt.Errorf("failed to create request for %s: %w", fullURL, err)
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("failed to fetch page %d: %w", i+1, err)
+		}
+
+		location := locationAreaAPIResponse{}
+		if err := json.NewDecoder(resp.Body).Decode(&location); err != nil {
+			return fmt.Errorf("Couldn't decode response body: %w", err)
+		}
+
+		resp.Body.Close()
+
+		fmt.Println(location.Location.Name)
+	}
+
+	return nil
+}
--- repl.go
+++ repl.go
@@ -66,5 +66,10 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the names of 20 location areas in the Pokemon world.",
+			callback:    commandMap,
+		},
 	}
 }
